af82326d69039b7ac2dc32a977ed3e1c3ce3e705
Charles Burt <charlesburt2000@gmail.com>
2023-10-27 09:36:21 +0100

    shortening code

--- 21_longest_word/Main.py
+++ 21_longest_word/Main.py
@@ -2,9 +2,6 @@ import os
 import re
 from pprint import pprint
 
-folder_path = "/Users/charlesburt/Library/CloudStorage/GoogleDrive-charlesburt2000@gmail.com/My Drive/Turing College/python_workout/21_longest_word/books"
-file = "43-0.txt"
-
 
 def clean(string):
     a = re.sub("http", " ", string)
@@ -14,65 +11,22 @@ def clean(string):
     return a
 
 
-def find_longest_word_in_line(file_name: str, line_num: int) -> str:
+def find_longest_word(filename):
     longest_word = ""
-    for file in os.listdir(folder_path):
-        if file == (file_name):
-            # print("Found file with filename specified")
-            with open(os.path.join(folder_path, file), "r") as f:
-                content = f.read()
-                line = content.splitlines()[line_num]
-                line = clean(line)
-                words_in_line = line.split()
-                num_letters_in_words = {len(word): word for word in words_in_line}
-                index_with_max_letters = max(num_letters_in_words.keys())
-
-                longest_word = (
-                    str(index_with_max_letters)
-                    + num_letters_in_words[index_with_max_letters]
-                )
-
-                break
-        else:
-            longest_word = "Didn't find matching file for specified filename"
-
+    for one_line in open(filename):
+        one_line = clean(one_line)
+        for one_word in one_line.split():
+            if len(longest_word) < len(one_word):
+                longest_word = one_word
     return longest_word
 
 
-# print(find_longest_word_in_line(file, 10))
+def find_all_longest_words(filepath):
+    return {
+        filename: find_longest_word(os.path.join(filepath, filename))
+        for filename in os.listdir(filepath)
+        if os.path.isfile(os.path.join(filepath, filename))
+    }
 
 
-def find_longest_word_in_file(file_name: str) -> dict:
-    """
-    Needs to be updated to read line by line, rather than the whole file at once.
-    Otherwise it will take up lots of memory for large files.
-    """
-    for file in os.listdir(folder_path):
-        if file == (file_name):
-            # print("Found file with filename specified")
-            with open(os.path.join(folder_path, file), "r") as f:
-                content = f.read()
-                content = clean(content)
-                words_in_content = content.split()
-                num_letters_in_words = {len(word): word for word in words_in_content}
-                index_with_max_letters = max(num_letters_in_words.keys())
-                return {
-                    file_name: str(index_with_max_letters)
-                    + " "
-                    + num_letters_in_words[index_with_max_letters]
-                }
-
-
-# print(find_longest_word_in_file(file))
-
-
-def find_all_longest_words(dir_name: str) -> dict:
-    my_dict = {}
-    for file in os.listdir(dir_name):
-        if file.endswith(".txt"):
-            d = find_longest_word_in_file(file)
-            my_dict[file] = d[file]
-    return my_dict
-
-
-pprint(find_all_longest_words(folder_path))
+pprint(find_all_longest_words("books/."))
